01ce6178c767bc0a51975e9a8b861ff4261d605f
roelderickx <roel.derickx@gmail.com>
2020-06-14 17:02:13 +0200

    Add separate primitive group classes for nodes, ways and relations

--- ogr2pbf/pbf_datawriter.py
+++ ogr2pbf/pbf_datawriter.py
@@ -10,32 +10,36 @@ import ogr2pbf.osmformat_pb2 as osmprotobuf
 
 # https://wiki.openstreetmap.org/wiki/PBF_Format
 
-class PbfPrimitiveBlock:
+class PbfPrimitiveGroup:
     def __init__(self):
         self.stringtable = {}
-        self.__add_string("")
-        
-        self.node_primitive_group = osmprotobuf.PrimitiveGroup()
-        self.ways_primitive_group = osmprotobuf.PrimitiveGroup()
-        self.relations_primitive_group = osmprotobuf.PrimitiveGroup()
+        self._add_string("")
         
         self.granularity = 100
         self.lat_offset = 0
         self.lon_offset = 0
         self.date_granularity = 1000
         
-        self.__last_id = 0
-        self.__last_lat = 0
-        self.__last_lon = 0
+        self.primitive_group = osmprotobuf.PrimitiveGroup()
     
     
-    def __add_string(self, s):
+    def _add_string(self, s):
         if not s in self.stringtable:
             index = len(self.stringtable)
             self.stringtable[s] = index
             return index
         else:
             return self.stringtable[s]
+
+
+
+class PbfPrimitiveGroupDenseNodes(PbfPrimitiveGroup):
+    def __init__(self):
+        super(PbfPrimitiveGroupDenseNodes, self).__init__()
+        
+        self.__last_id = 0
+        self.__last_lat = 0
+        self.__last_lon = 0
     
     
     def __lat_to_pbf(self, lat):
@@ -50,43 +54,55 @@ class PbfPrimitiveBlock:
         pbflat = self.__lat_to_pbf(osmpoint.y)
         pbflon = self.__lon_to_pbf(osmpoint.x)
 
-        self.node_primitive_group.dense.id.append(osmpoint.id - self.__last_id)
-        self.node_primitive_group.dense.lat.append(pbflat - self.__last_lat)
-        self.node_primitive_group.dense.lon.append(pbflon - self.__last_lon)
+        self.primitive_group.dense.id.append(osmpoint.id - self.__last_id)
+        self.primitive_group.dense.lat.append(pbflat - self.__last_lat)
+        self.primitive_group.dense.lon.append(pbflon - self.__last_lon)
         
         self.__last_id = osmpoint.id
         self.__last_lat = pbflat
         self.__last_lon = pbflon
         
         for (key, value) in osmpoint.tags.items():
-            self.node_primitive_group.dense.keys_vals.append(self.__add_string(key))
-            self.node_primitive_group.dense.keys_vals.append(self.__add_string(value))
-        self.node_primitive_group.dense.keys_vals.append(0)
-    
+            self.primitive_group.dense.keys_vals.append(self._add_string(key))
+            self.primitive_group.dense.keys_vals.append(self._add_string(value))
+        self.primitive_group.dense.keys_vals.append(0)
+
+
+
+class PbfPrimitiveGroupWays(PbfPrimitiveGroup):
+    def __init__(self):
+        super(PbfPrimitiveGroupWays, self).__init__()
+        
     
     def add_way(self, osmway):
         way = osmprotobuf.Way()
         way.id = osmway.id
         
         for (key, value) in osmway.tags.items():
-            way.keys.append(self.__add_string(key))
-            way.vals.append(self.__add_string(value))
+            way.keys.append(self._add_string(key))
+            way.vals.append(self._add_string(value))
         
         prev_node_id = 0
         for node in osmway.points:
             way.refs.append(node.id - prev_node_id)
             prev_node_id = node.id
         
-        self.ways_primitive_group.ways.append(way)
+        self.primitive_group.ways.append(way)
     
+    
+
+class PbfPrimitiveGroupRelations(PbfPrimitiveGroup):
+    def __init__(self):
+        super(PbfPrimitiveGroupRelations, self).__init__()
+        
     
     def add_relation(self, osmrelation):
         relation = osmprotobuf.Relation()
         relation.id = osmrelation.id
         
         for (key, value) in osmrelation.tags.items():
-            relation.keys.append(self.__add_string(key))
-            relation.vals.append(self.__add_string(value))
+            relation.keys.append(self._add_string(key))
+            relation.vals.append(self._add_string(value))
         
         prev_member_id = 0
         for member in osmrelation.members:
@@ -100,25 +116,7 @@ class PbfPrimitiveBlock:
                 relation_type = osmprotobuf.MemberType.RELATION
             relation.types.append(relation_type)
         
-        self.relations_primitive_group.relations.append(relation)
-
-
-    def get_primitive_block(self):
-        logging.debug("Primitive block generation")
-        primitive_block = osmprotobuf.PrimitiveBlock()
-        for (string, index) in sorted(self.stringtable.items(), key=lambda kv: kv[1]):
-            primitive_block.stringtable.s.append(string.encode('utf-8'))
-        
-        primitive_block.primitivegroup.append(self.node_primitive_group)
-        primitive_block.primitivegroup.append(self.ways_primitive_group)
-        primitive_block.primitivegroup.append(self.relations_primitive_group)
-        
-        primitive_block.granularity = self.granularity
-        primitive_block.lat_offset = self.lat_offset
-        primitive_block.lon_offset = self.lon_offset
-        primitive_block.date_granularity = self.date_granularity
-        
-        return primitive_block
+        self.primitive_group.relations.append(relation)
 
 
 
@@ -132,7 +130,7 @@ class PbfDataWriter(DataWriterBase):
         self.f = open(self.filename, 'wb', buffering = -1)
     
     
-    def __write_blob(self, data, block_type="OSMData"):
+    def __write_blob(self, data, block_type):
         logging.debug("Writing blob, type = %s" % block_type)
         
         blob = fileprotobuf.Blob()
@@ -155,35 +153,53 @@ class PbfDataWriter(DataWriterBase):
     def write_header(self):
         logging.debug("Writing file header")
         
-        headerblock = osmprotobuf.HeaderBlock()
-        headerblock.required_features.append("OsmSchema-V0.6")
-        headerblock.required_features.append("DenseNodes")
-        headerblock.writingprogram = "ogr2pbf %s" % self.get_version()
-        self.__write_blob(headerblock.SerializeToString(), "OSMHeader")
+        header_block = osmprotobuf.HeaderBlock()
+        header_block.required_features.append("OsmSchema-V0.6")
+        header_block.required_features.append("DenseNodes")
+        header_block.writingprogram = "ogr2pbf %s" % self.get_version()
+        self.__write_blob(header_block.SerializeToString(), "OSMHeader")
+
+
+    def __write_primitive_block(self, pbf_primitive_group):
+        logging.debug("Primitive block generation")
+        
+        primitive_block = osmprotobuf.PrimitiveBlock()
+        # add stringtable
+        for (string, index) in sorted(pbf_primitive_group.stringtable.items(), key=lambda kv: kv[1]):
+            primitive_block.stringtable.s.append(string.encode('utf-8'))
+        # add geometries
+        primitive_block.primitivegroup.append(pbf_primitive_group.primitive_group)
+        # set parameters
+        primitive_block.granularity = pbf_primitive_group.granularity
+        primitive_block.lat_offset = pbf_primitive_group.lat_offset
+        primitive_block.lon_offset = pbf_primitive_group.lon_offset
+        primitive_block.date_granularity = pbf_primitive_group.date_granularity
+        # write primitive block
+        self.__write_blob(primitive_block.SerializeToString(), "OSMData")
 
     
     def write_nodes(self, nodes):
         logging.debug("Writing nodes")
-        pbf_primitive_block = PbfPrimitiveBlock()
+        primitive_group = PbfPrimitiveGroupDenseNodes()
         for node in nodes:
-            pbf_primitive_block.add_node(node)
-        self.__write_blob(pbf_primitive_block.get_primitive_block().SerializeToString())
+            primitive_group.add_node(node)
+        self.__write_primitive_block(primitive_group)
     
     
     def write_ways(self, ways):
         logging.debug("Writing ways")
-        pbf_primitive_block = PbfPrimitiveBlock()
+        primitive_group = PbfPrimitiveGroupWays()
         for way in ways:
-            pbf_primitive_block.add_way(way)
-        self.__write_blob(pbf_primitive_block.get_primitive_block().SerializeToString())
+            primitive_group.add_way(way)
+        self.__write_primitive_block(primitive_group)
     
     
     def write_relations(self, relations):
         logging.debug("Writing relations")
-        pbf_primitive_block = PbfPrimitiveBlock()
+        primitive_group = PbfPrimitiveGroupRelations()
         for relation in relations:
-            pbf_primitive_block.add_relation(relation)
-        self.__write_blob(pbf_primitive_block.get_primitive_block().SerializeToString())
+            primitive_group.add_relation(relation)
+        self.__write_primitive_block(primitive_group)
     
     
     def close(self):
