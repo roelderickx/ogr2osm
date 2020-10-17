222ee1736b587aa1f785fa8821cc02b2997f010d
roelderickx <roel.derickx@gmail.com>
2020-10-17 10:07:22 +0200

    Update protobuf

--- protobuf/create_protobuf.sh
+++ protobuf/create_protobuf.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 rm fileformat.proto ../ogr2pbf/fileformat_pb2.py
-echo 'syntax = "proto2";' > fileformat.proto
 curl -L https://github.com/openstreetmap/OSM-binary/raw/master/src/fileformat.proto >> fileformat.proto
 protoc --python_out=../ogr2pbf ./fileformat.proto
 
 rm osmformat.proto ../ogr2pbf/osmformat_pb2.py
-echo 'syntax = "proto2";' > osmformat.proto
 curl -L https://github.com/openstreetmap/OSM-binary/raw/master/src/osmformat.proto >> osmformat.proto
 protoc --python_out=../ogr2pbf ./osmformat.proto
 
