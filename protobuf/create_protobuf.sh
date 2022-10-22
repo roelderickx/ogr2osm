d26b790e18a9243e36578b79aa248d56c00fb04f
roelderickx <roel.derickx@gmail.com>
2022-10-22 09:57:45 +0200

    #22 Upgrade protobuf

--- protobuf/create_protobuf.sh
+++ protobuf/create_protobuf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm fileformat.proto ../ogr2pbf/fileformat_pb2.py
-curl -L https://github.com/openstreetmap/OSM-binary/raw/master/src/fileformat.proto >> fileformat.proto
-protoc --python_out=../ogr2pbf ./fileformat.proto
+rm fileformat.proto ../ogr2osm/fileformat_pb2.py
+curl -L https://raw.githubusercontent.com/openstreetmap/OSM-binary/master/osmpbf/fileformat.proto >> fileformat.proto
+protoc --python_out=../ogr2osm ./fileformat.proto
 
-rm osmformat.proto ../ogr2pbf/osmformat_pb2.py
-curl -L https://github.com/openstreetmap/OSM-binary/raw/master/src/osmformat.proto >> osmformat.proto
-protoc --python_out=../ogr2pbf ./osmformat.proto
+rm osmformat.proto ../ogr2osm/osmformat_pb2.py
+curl -L https://raw.githubusercontent.com/openstreetmap/OSM-binary/master/osmpbf/osmformat.proto >> osmformat.proto
+protoc --python_out=../ogr2osm ./osmformat.proto
 
